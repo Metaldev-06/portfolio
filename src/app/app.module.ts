98dea3dbacc6abbdea0b4c67404b54e6cad06ba6
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-08-23 23:13:17 -0300

    Version 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,

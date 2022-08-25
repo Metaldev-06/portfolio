624bf99c985ca30d2c9cb30f57f6ed0ec66e636b
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-08-25 00:07:44 -0300

    Version 0.6

--- src/app/components/home/proyectos/proyectos.component.ts
+++ src/app/components/home/proyectos/proyectos.component.ts
@@ -17,31 +17,31 @@ export class ProyectosComponent{
   proyectos: Proyectos[] = [
     {
       url: 'https://giftify-app.netlify.app/',
-      img: '../../../../../assets/img/proyecto1.png',
+      img: '../../../../../assets/img/proyecto1_optimized.png',
       title: 'GifTify',
       description: 'Es una página web para buscar gif, usando la API de Giphy. Desarrollada con Angular, Sass y TypeScript.'
     },
     {
       url: 'https://candela-fest.netlify.app/',
-      img: '../../../../../assets/img/proyecto2.png',
+      img: '../../../../../assets/img/proyecto2_optimized.png',
       title: 'Candela Fest',
       description: 'Es una tarjeta de invitación digital para un XI años de Candela Rojas. Desarrollada con HTML, JavaScript y Sass.'
     },
     {
       url: 'https://metaldev-06.github.io/Juego-del-ahorcado-Alura-Challenges-Oracle-ONE/',
-      img: '../../../../../assets/img/proyecto3.png',
+      img: '../../../../../assets/img/proyecto3_optimized.png',
       title: 'Juego del Ahorcado',
       description: 'Es el clásico juego del ahorcado, este es un proyecto de Oracle Next Education. Desarrollado con HTML, CSS y JavaScript.'
     },
     {
       url: 'https://metaldev-06.github.io/Encriptador-de-texto-Alura-Challenges-Oracle-ONE/',
-      img: '../../../../../assets/img/proyecto4.png',
+      img: '../../../../../assets/img/proyecto4_optimized.png',
       title: 'Encriptador de Texto',
       description: 'Es un sencillo encriptador y desencriptador de texto, este es un proyecto de Oracle Next Education. Desarrollado con HTML, CSS y JavaScript.'
     },
     {
       url: 'https://color-selector-metaldev.netlify.app/',
-      img: '../../../../../assets/img/proyecto5.png',
+      img: '../../../../../assets/img/proyecto5_optimized.png',
       title: 'Color Picker',
       description: 'Es un sencillo selector de color que fue desarrollado con el proposito de aprender nuevas cosas. Desarrollado con HTML, CSS y JavaScript.'
     },

87c6158ff333396327719b5263e4ac8c4d50b16d
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-08-25 02:41:40 -0300

    Version 0.7

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Scroll } from '@angular/router';
 import { HeaderService } from 'src/app/service/header.service';
 
 @Component({
@@ -6,15 +7,27 @@ import { HeaderService } from 'src/app/service/header.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+
+export class HeaderComponent {
+  
   constructor(private headerService: HeaderService) { }
   get mostrar() {
     return this.headerService.mostrar();
   }
-
+  
   activarSidebar() {
     this.headerService.activadorSidebar.emit(this.headerService.mostrar());
   }
-
+  
+  headerVariable: boolean = false;
+  
+  @HostListener('document:scroll')
+  scrollFunction() {
+    if(document.documentElement.scrollTop > 777) {
+      this.headerVariable = true;
+    } else {
+      this.headerVariable = false;
+    }
+  }
 }
